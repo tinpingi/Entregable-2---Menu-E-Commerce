bf7cea8d169be5ab77a6ca9576f6eba94ca35286
tinpingi <86984691+tinpingi@users.noreply.github.com>
2022-08-22 14:38:19 -0300

    menu e-commerce

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+
+//components
+import NavBar from './Components/NavBar/NavBar';
+
+function App () {
+  
+ return (
+      <div className="App">
+ <NavBar />
+  
+      </div>
+    );
+  
+  }
+ 
+
+
+export default App;
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <div className ='NavBar'>
+            <ul>
+          
+            
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li><a href="#">Carrito</a></li>
+        </ul>
+       
+        </div>
+    )
+}
+
+
+export default NavBar;
